f9d72e4181c6fc9bcad5f38042bb0b1a3d33fbdf
LhMinh2607 <workatlhm2607@gmail.com>
2022-04-30 21:17:07 +0700

    added a lot more to the table including reminder, show user info, noting mood, noting userId and configured the bot to have tracker_store and socketio

--- actions/actions.py
+++ actions/actions.py
@@ -8,36 +8,65 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
+from psycopg2 import Date
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import ReminderScheduled, ReminderCancelled, SlotSet, AllSlotsReset
 
 from urllib.request import urlopen
 import json
 import random
+
+import pymongo 
+import datetime
+
+from bson.objectid import ObjectId
+
 url = ["https://api.pushshift.io/reddit/search/submission/?subreddit=crappydesign&sort=desc&sort_type=score&size=100&user_removed=false&mod_removed=false&over_18=false&is_video=false&is_original_content=true", 
 "https://api.pushshift.io/reddit/search/submission/?subreddit=programmerhumor&sort=desc&sort_type=score&size=100&user_removed=false&mod_removed=false&over_18=false&is_video=false&is_original_content=true",
 "https://api.pushshift.io/reddit/search/submission/?subreddit=memes&sort=desc&sort_type=score&size=100&user_removed=false&mod_removed=false&over_18=false&is_video=false&is_original_content=true",
 "https://api.pushshift.io/reddit/search/submission/?subreddit=artbreeder&sort=desc&sort_type=score&size=100&user_removed=false&mod_removed=false&over_18=false&is_video=false&is_original_content=true",
 "https://api.pushshift.io/reddit/search/submission/?subreddit=wholesomememes&sort=desc&sort_type=score&size=100&user_removed=false&mod_removed=false&over_18=false&is_video=false&is_original_content=true"]
 
+mentalBotDB = pymongo.MongoClient("mongodb://localhost:27017/")
+mentalBot = mentalBotDB["mentalBotDB"]
 
-  
+usersCollection = mentalBot["users"]
+
+remindersCollection = mentalBot["reminders"]
+
+userId = ""
 
 
-
-class ActionHelloWorld(Action):
+class ActionInitialGreeting(Action):
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_initial_greeting"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        SlotSet("userId", None)
 
-        dispatcher.utter_message(text="Hello World!")
+        entities = tracker.latest_message.get("entities")
 
-        return []
+        userId = entities[0]['value']
+
+        print(userId)
+
+        usersCollection = mentalBot["users"]
+
+        user = usersCollection.find_one({"_id": ObjectId(str(userId))})
+
+        print(user['username'])
+
+        print("action_initial_greeting get called")
+
+        dispatcher.utter_message(response="utter_greet")
+
+        return[SlotSet("user", str(user['username'])), SlotSet("userId", str(user['_id']))]
+
 
 class ActionSendPicture(Action):
         
@@ -65,5 +94,226 @@ class ActionSendPicture(Action):
             dispatcher.utter_message(response="utter_send_picture")
             dispatcher.utter_message(image=reddit_post['data'][index]['url'])
             dispatcher.utter_message(text=f"Nguồn: "+reddit_post['data'][index]['full_link'])
+
+        return[]
+
+# class ActionAskForTimer(Action):
+#     """Schedules a reminder, supplied with the last message's entities."""
+
+#     def name(self) -> Text:
+#         return "action_ask_for_timer"
+
+#     async def run(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any],
+#     ) -> List[Dict[Text, Any]]:
+
+#         # next(tracker.get_latest_entity_values(entity_type="timer", entity_role="hours"))
+
+#         dispatcher.utter_message("Mình sẽ nhắn lại cho bạn trong 5s. (Đang test)")
+#         date = datetime.datetime.now() + datetime.timedelta(hours=1)
+
+#         entities = tracker.latest_message.get("entities")
+
+#         print(tracker.latest_message)
+#         print(tracker.latest_message.get("entities"))
+#         print(entities)
+
+
+#         mydict = { "userId": "6258e2b0ee1e676f8626d4bd", "username": "LhMinh2607", "reminder": entities, "createdAt": datetime.datetime.now(), "duration": 3600}
+
+#         remindersCollection.insert_one(mydict)
+
+#         # SlotSet("activity", None) #reset "activity" slot
+#         # AllSlotsReset()
+
+#         reminder = ReminderScheduled(
+#             "EXTERNAL_reminder",
+#             trigger_date_time=date,
+#             entities=entities,
+#             name="my_reminder",
+#             kill_on_user_message=False,
+#         )
+
+#         return [reminder]
+
+
+class ActionSetReminder(Action):
+    """Schedules a reminder, supplied with the last message's entities."""
+
+    def name(self) -> Text:
+        return "action_set_reminder"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        # next(tracker.get_latest_entity_values(entity_type="timer", entity_role="hours"))
+
+        userId = tracker.get_slot("userId")
+        
+        entities = tracker.latest_message.get("entities")
+
+        print(tracker.latest_message)
+        print(tracker.latest_message.get("entities"))
+        print(entities[0]["value"])
+        print(entities[1]["value"])
+
+
+        if entities[1] != "None":
+            duration = entities[1]['value']
+        else:
+            duration="1h00m00s"
+        print(duration)
+
+        splitTimerHour = duration.split('h')
+        splitTimerMinute = splitTimerHour[1].split('m')
+        splitTimerSecond = splitTimerMinute[1].split('s')
+
+        print(splitTimerHour)
+        print(splitTimerMinute)
+        print(splitTimerSecond)
+
+        date = datetime.datetime.now() + datetime.timedelta(hours= int(splitTimerHour[0]), minutes=int(splitTimerMinute[0]), seconds=int(splitTimerSecond[0]))
+
+
+        mydict = { "userId": str(userId), "username": "Nam", "reminder": entities[0], "createdAt": datetime.datetime.now(), "duration": entities[1]}
+
+        remindersCollection.insert_one(mydict)
+
+        dispatcher.utter_message("Mình sẽ nhắn lại cho bạn trong " + entities[1]['value'] + " (Beta)")
+
+
+        # SlotSet("activity", None) #reset "activity" slot
+        # AllSlotsReset()
+
+        reminder = ReminderScheduled(
+            "EXTERNAL_reminder",
+            trigger_date_time=date,
+            entities=entities[0],
+            name="my_reminder",
+            kill_on_user_message=False,
+        )
+
+        return [reminder]
+
+
+class ActionReactToReminder(Action):
+    """Reminds the user to call someone."""
+
+    def name(self) -> Text:
+        return "action_remind"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+    
+        userId = tracker.get_slot("userId")
+        # for obj in remindersCollection.find().sort([('createdAt', -1)]):
+        #     if obj['userId'] == "6258e2b0ee1e676f8626d4bd":
+        #         # activity = next(tracker.get_slot("activity"), None)
+        #         activity = obj["reminder"][0]["value"]
+        #         print(activity)
+        #         dispatcher.utter_message(response="utter_reminder_message")
+
+        for obj in remindersCollection.find().sort([('createdAt', -1)]):
+            if obj['userId'] == str(userId):
+                # activity = next(tracker.get_slot("activity"), None)
+                activity = obj["reminder"]["value"]
+                print(activity)
+                dispatcher.utter_message(response="utter_reminder_message")
+                break
+        return []
+
+
+class ActionCancelReminder(Action):
+    def name(self):
+        return "action_cancel_reminder"
+
+    def run(self, dispatcher, tracker, domain):  
+        ####some code or no code hereSlotSet("activity", None)
+        return[ReminderCancelled]
+
+
+class ActionShowUserInfo(Action):
+
+    def name(self) -> Text:
+        return "action_show_user_info"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        userId = tracker.get_slot("userId")
+
+        dispatcher.utter_message(response="utter_respond_to_user_info_req")
+
+        usersCollection = mentalBot["users"]
+
+        user = usersCollection.find_one({"_id": ObjectId(str(userId))})
+
+        # dispatcher.utter_message(text=f"Tên đầy đủ của bạn là "+user['name'])
+        # dispatcher.utter_message(text=f"Username của bạn là "+user['username'])
+        # dispatcher.utter_message(text=f"Nghề nghiệp của bạn là "+user['occupation'])
+        # dispatcher.utter_message(text=f"Số điện thoại của bạn là "+user['phoneNumber'])
+        # dispatcher.utter_message(text=f"Email của bạn là "+user['email'])
+        # dispatcher.utter_message(text=f"Ngày tháng năm sinh của bạn là "+str(user['dob'])[:10])
+        # # dispatcher.utter_message(text=f"Bạn "+datetime.today().year - user['dob'][:4]+" tuổi")
+        # dispatcher.utter_message(text=f"Tâm trạng hiện tại của bạn do bạn cung cấp là đang "+user['mood'])
+        # dispatcher.utter_message(text=f"Tiến độ của bạn là "+user['progress'])
+
+        print(user['name'])
+        print(user['username'])
+        print(user['occupation'])
+        print(user['phoneNumber'])
+        print(user['email'])
+        print(user['dob'])
+        print(user['mood'])
+        print(user['progress'])
+
+        dispatcher.utter_message(text=f"Tên đầy đủ của bạn là "+str(user['name'])+"\n"+
+        "Username của bạn là "+str(user['username'])+"\n"+
+        "Nghề nghiệp của bạn là "+str(user['occupation'])+"\n"+
+        "Số điện thoại của bạn là "+str(user['phoneNumber'])+"\n"+
+        "Email của bạn là "+str(user['email'])+"\n"+
+        "Ngày tháng năm sinh của bạn là "+str(user['dob'])[:10]+"\n"+
+        "Tâm trạng hiện tại của bạn do bạn cung cấp là đang "+str(user['mood'])+"\n"
+        "Tiến độ của bạn là "+str(user['progress'])+"\n")
+
+        return[]
+
+
+class ActionNoteMood(Action):
+    def name(self):
+        return "action_note_mood"
+
+    def run(self, dispatcher, tracker, domain):
+        userId = tracker.get_slot("userId") 
+        mood = tracker.latest_message.get("entities") 
+
+
+        print("action_node_mood is called")
+        print(mood[0]["value"])
+        usersCollection.find_one_and_update({'_id': ObjectId(str(userId))}, update={"$set": {'mood': mood[0]["value"]}})
+
+        dispatcher.utter_message(response="action_note_mood")
+
+        ####some code or no code here SlotSet("activity", None)
+        return[]
+
+class FollowUpMood(Action):
+    def name(self):
+        return "action_follow_up_mood"
+
+    def run(self, dispatcher, tracker, domain):
+        #follow up to user, ask them about their previous mood log
 
         return[]
--- database/connection.py
+++ database/connection.py
@@ -1,5 +1,9 @@
 import sys
 import pymongo 
+from datetime import datetime
+from bson.objectid import ObjectId
+
+
 
 # Replace the uri string with your MongoDB deployment's connection string.
 conn_str = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
@@ -7,42 +11,119 @@ conn_str = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20
 # set a 5-second connection timeout
 client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
 try:
-    print(client.server_info())
+    # print(client.server_info())
     print('Connected to MongoDB via PyMongo')
 except Exception:
     print("Unable to connect to the server.")
 
 
-rasaDB = pymongo.MongoClient("mongodb://localhost:27017/")
+# rasaDB = pymongo.MongoClient("mongodb://localhost:27017/")
 
-rasa = rasaDB["rasa"]
+# rasa = rasaDB["rasa"]
 
-print(rasaDB.list_database_names())
+# print(rasaDB.list_database_names())
 
-dblist = rasaDB.list_database_names()
-if "rasa" in dblist:
-    print("The database exists.")
-
-
-rasaDB = pymongo.MongoClient("mongodb://localhost:27017/")
-rasa = rasaDB["rasa"]
-
-customers = rasa["customers"]
-
-print(rasa.list_collection_names())
-
-collist = rasa.list_collection_names()
-if "customers" in collist:
-    print("The collection exists.")
-else:
-    print("The collection doesn't exists")
+# dblist = rasaDB.list_database_names()
+# if "rasa" in dblist:
+#     print("The database exists.")
 
 
+# rasaDB = pymongo.MongoClient("mongodb://localhost:27017/")
+# rasa = rasaDB["rasa"]
 
-testCol = rasa["test"]
+# customers = rasa["customers"]
 
-mydict = { "name": "John", "address": "Highway 37" }
+# print(rasa.list_collection_names())
 
-x = testCol.insert_one(mydict)
+# collist = rasa.list_collection_names()
+# if "customers" in collist:
+#     print("The collection exists.")
+# else:
+#     print("The collection doesn't exists")
 
 
+
+# testCol = rasa["test"]
+
+# mydict = { "name": "John", "address": "Highway 37" }
+
+# x = testCol.insert_one(mydict)
+
+mentalBotDB = pymongo.MongoClient("mongodb://localhost:27017/")
+mentalBot = mentalBotDB["mentalBotDB"]
+
+remindersCollection = mentalBot["reminders"]
+
+# entities = [{"entity": "activity",
+# "start":  5,
+# "end": 6,
+# "confidence_entity": 0.6445619463920593,
+# "value": "đập đồ",
+# "extractor": "DIETClassifier"}]
+
+# mydict = { "userId": "6258e2b0ee1e676f8626d4bd", "username": "LhMinh2607", "reminder": entities, "createdAt": datetime.today()}
+
+# remindersCollection.insert_one(mydict)
+
+# for obj in remindersCollection.find().sort([('createdAt', -1)]):
+#     if obj['userId'] == "6258e2b0ee1e676f8626d4bd":
+#         # activity = next(tracker.get_slot("activity"), None)
+#         activity = obj["reminder"][0]["value"]
+#         print(activity)
+#         break
+        
+
+usersCollection = mentalBot["users"]
+
+user = usersCollection.find_one({"_id": ObjectId("6258e2b0ee1e676f8626d4bd")})
+
+print("Tên đầy đủ của bạn là "+user['name'])
+print("Username của bạn là "+user['username'])
+print("Nghề nghiệp của bạn là "+user['occupation'])
+print("Số điện thoại của bạn là "+user['phoneNumber'])
+print("Email của bạn là "+user['email'])
+print("Ngày tháng năm sinh của bạn là "+str(user['dob'])[:10])
+print("Tâm trạng hiện tại của bạn do bạn cung cấp là đang "+user['mood'])
+print("Tiến độ của bạn là "+user['progress'])
+print(user["username"]+" có thể thay đổi thông tin trên trang cá nhân của bạn bất cứ lúc nào")
+
+
+# print(user['username'])
+
+## reminder is an array
+# for obj in remindersCollection.find().sort([('createdAt', -1)]):
+#     if obj['userId'] == "6258e2b0ee1e676f8626d4bd":
+#         # activity = next(tracker.get_slot("activity"), None)
+#         for act in obj["reminder"]:
+#             activity = act["value"]
+#             print(activity)
+#         break
+
+# timer = '1h20m30s'
+# splitTimerHour = timer.split('h')
+# splitTimerMinute = splitTimerHour[1].split('m')
+# splitTimerSecond = splitTimerMinute[1].split('s')
+# print(splitTimerHour)
+# print(splitTimerMinute)
+# print(splitTimerSecond)
+
+# hours = timer.replace('h', '').replace('m', '')  
+# print(hours)
+
+# usersCollection = mentalBot['users']
+
+# usersCollection.find_one_and_update({'_id': ObjectId('6258e2b0ee1e676f8626d4bd')}, update={"$set": {'mood': "vui quá trời quá đất"}})
+
+# for obj in usersCollection.find().sort([('createdAt', -1)]):
+#     if obj['_id'] == ObjectId("6258e2b0ee1e676f8626d4bd"):
+#         print(obj["mood"])
+#         break
+
+
+# reminder is an object
+# for obj in remindersCollection.find().sort([('createdAt', -1)]):
+#     if obj['userId'] == "6258e2b0ee1e676f8626d4bd":
+#         # activity = next(tracker.get_slot("activity"), None)
+#         activity = obj["reminder"]["value"]
+#         print(activity)
+#         break
